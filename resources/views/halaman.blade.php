6a89378c21a8e255589d0bb6536c84c3cae67d6c
nabilasyifahrp <nabilasyifahrp07@gmail.com>
2025-04-24 10:28:10 +0700

    Update

--- resources/views/halaman.blade.php
+++ resources/views/halaman.blade.php
@@ -5,13 +5,10 @@
   <title>Halaman Setelah Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Poppins Font -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
-  <!-- Font Awesome Tanpa Integrity -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
   <style>
@@ -72,7 +69,6 @@
 </head>
 <body>
 
-  <!-- Icon panah kembali -->
   <a href="{{ route('dashboard') }}" class="back-icon" title="Kembali ke Dashboard">
     <i class="fas fa-arrow-left"></i>
   </a>
